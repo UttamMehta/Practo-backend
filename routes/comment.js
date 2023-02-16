dded3b0aa40331f6eb777d6e71baf65b2aebda59
UttamMehta <binobajichiku@gmail.com>
2023-02-16 21:27:05 +0530

    Added all function for practo backend

--- routes/comment.js
+++ routes/comment.js
@@ -1,11 +1,13 @@
-
-const express = require('express');
-const { getCommentsByPostId, addCommentForPost } = require('../controllers/comment');
-const authMiddleware = require('../middlewares/auth');
+const express = require("express");
+const {
+  getCommentsByPostId,
+  addCommentForPost,
+} = require("../controllers/comment");
+const authMiddleware = require("../middlewares/auth");
 
 const commentRouter = express.Router();
 
-commentRouter.post('/', authMiddleware, addCommentForPost);
-commentRouter.get('/post/:postId', getCommentsByPostId)
+commentRouter.post("/", authMiddleware, addCommentForPost);
+commentRouter.get("/post/:postId", getCommentsByPostId);
 
 module.exports = commentRouter;
